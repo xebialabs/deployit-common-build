f022b398f654b78446685f8d7b41da4c23ad5459
hierynomus <jeroen@hierynomus.com>
2011-11-18 13:52:15 +0100

    Added more DSL

--- DeployitCommonBuild.scala
+++ DeployitCommonBuild.scala
@@ -12,9 +12,10 @@ object PluginBuild extends Plugin {
 
 object DeployitCommonBuild extends Plugin {
 
+  def itest(projectRefs: Seq[Project]) = TaskKey[Unit]("itest","Run Itests") <<= projectRefs.map(ref => test in PluginBuild.Itest in ref).dependOn
+
   val buildSettings = Defaults.defaultSettings ++ Seq(
     organization := "com.xebialabs.deployit",
-    version := "3.6.0-SNAPSHOT",
     autoScalaLibrary := false,
     crossPaths := false,
     shellPrompt := ShellPrompt.buildShellPrompt,
@@ -28,9 +29,10 @@ object DeployitCommonBuild extends Plugin {
  
   object PluginProject {
     import PluginBuild._
-    def apply(name: String, settings: Seq[Setting[_]]) = Project(
+    def apply(name: String, settings: Seq[Setting[_]]): Project = PluginProject(name, file(name), settings)
+    def apply(name: String, file: File, settings: Seq[Setting[_]]) = Project(
       name, 
-      file(name),
+      file,
       settings = buildSettings ++ pluginSettings ++ Seq(
         organization := "com.xebialabs.deployit.plugins"
       ) ++ settings
@@ -46,6 +48,7 @@ object DeployitCommonBuild extends Plugin {
       ExclusionRule(organization = "junit")
     )}
 
+    val jackrabbit = Seq(jackrabbitApi, jackrabbitCore)
     val resteasy = excludeFromAll(Seq(resteasyJaxrs, resteasyMultipart, resteasyJaxb))
     val standardDeps = Seq(slf4jApi, guava)
     val springDeps = Seq(springCore, springSecurity)
@@ -56,6 +59,7 @@ object DeployitCommonBuild extends Plugin {
   object Dependency {
     object V {
       val Api = "3.6.0-beta-14"
+      val Jackrabbit = "2.2.0"
       val Jython = "2.5.1"
       val Overthere = "1.0.7"
       val Resteasy = "2.0.0.GA"
@@ -66,15 +70,24 @@ object DeployitCommonBuild extends Plugin {
 
     val args4j = "args4j" % "args4j" % "2.0.16"
     val commonsLang = "commons-lang" % "commons-lang" % "2.5"
+    val deployitJeePlugin = "com.xebialabs.deployit.plugins" % "jee-plugin" % V.Api
+    val deployitOvertherePlugin = "com.xebialabs.deployit.plugins" % "overthere-plugin" % V.Api
     val deployitPluginApi = "com.xebialabs.deployit" % "udm-plugin-api" % V.Api
+    val deployitPlanner = "com.xebialabs.deployit" % "planner" % V.Api
+    val deployitPythonPlugin = "com.xebialabs.deployit.plugins" % "python-plugin" % V.Api
+    val deployitUdmServerApi = "com.xebialabs.deployit" % "udm-server-api" % V.Api
     val freemarker = "org.freemarker" % "freemarker" % "2.3.18"
     val guava = "com.google.guava" % "guava" % "10.0.1"
+    val jackrabbitApi = "org.apache.jackrabbit" % "jackrabbit-api" % V.Jackrabbit
+    val jackrabbitCore = "org.apache.jackrabbit" % "jackrabbit-core" % V.Jackrabbit
     val jaxRsApi = "org.jboss.resteasy" % "jaxrs-api" % V.Resteasy
+    val jcr = "javax.jcr" % "jcr" % "2.0"
     val jmustache = "com.samskivert" % "jmustache" % "1.3"
     val jython = "org.python" % "jython" % V.Jython
     val jythonStandalone = "org.python" % "jython-standalone" % V.Jython
     val overthere = "com.xebialabs.overthere" % "overthere" % V.Overthere
     val pegdown = "org.pegdown" % "pegdown" % "1.0.2"
+    val qrmediaCommons = "com.qrmedia.commons" % "commons-lang" % "1.0.2"
     val resteasyJaxrs = "org.jboss.resteasy" % "resteasy-jaxrs" % V.Resteasy 
     val resteasyMultipart = "org.jboss.resteasy" % "resteasy-multipart-provider" % V.Resteasy 
     val resteasyJaxb = "org.jboss.resteasy" % "resteasy-jaxb-provider" % V.Resteasy 
@@ -88,6 +101,7 @@ object DeployitCommonBuild extends Plugin {
     val truezipKernel = "de.schlichtherle.truezip" % "truezip-kernel" % V.Truezip
 
     object Test {
+      val deployitUdmTestSupport = "com.xebialabs.deployit" % "udm-test-support" % V.Api % "test"
       val itestSupport = "com.xebialabs.overthere" % "itest-support" % V.Overthere
       val junit = "junit" % "junit-dep" % "4.10" % "test"
       val junitInterface = "com.novocode" % "junit-interface" % "0.7" % "test"
