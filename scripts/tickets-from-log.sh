8777a5c68eaea6a9123b415bde558d06ea470a9a
Ric Klaren <rklaren@xebialabs.com>
2013-06-05 15:07:23 +0200

    Add script to quickly get JIRA issues from commit messages

---
+++ scripts/tickets-from-log.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+FROM=$1
+TO=$2
+if [ -z "$FROM" -o -z "$TO" ]; then
+	cat -<<EOF
+
+Usage: $(basename $0) <from-version> <to-version>
+
+This script expects to be run from the directory above the checked out
+source trees of plugin-api engine bundled-plugins and deployit.
+
+EOF
+	exit 1
+fi	
+for i in plugin-api engine bundled-plugins deployit; do
+	if [ ! -d "$i" ]; then
+		echo "Directory $i not found!"
+	else
+		( cd "$i" && git log ${i}-${FROM}..${i}-${TO} )
+	fi
+done | egrep -o 'DEPLOYIT(PB)?-[0-9]+' | sed s/DEPLOYIT-/DEPLOYITPB-/g | sort -u
