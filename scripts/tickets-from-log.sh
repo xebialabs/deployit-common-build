cb387a28d3f71e754787a5661456bcd7ae9209f5
Ric Klaren <rklaren@xebialabs.com>
2013-08-27 22:26:27 +0200

    tickets-from-log script support no end range

--- scripts/tickets-from-log.sh
+++ scripts/tickets-from-log.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 FROM=$1
 TO=$2
-if [ -z "$FROM" -o -z "$TO" ]; then
+if [ -z "$FROM" ]; then
 	cat -<<EOF
 
-Usage: $(basename $0) <from-version> <to-version>
+Usage: $(basename $0) <from-version> [to-version]
 
 This script expects to be run from the directory above the checked out
 source trees of plugin-api engine bundled-plugins and deployit.
@@ -16,6 +16,8 @@ for i in plugin-api engine bundled-plugins deployit; do
 	if [ ! -d "$i" ]; then
 		echo "Directory $i not found!"
 	else
-		( cd "$i" && git log ${i}-${FROM}..${i}-${TO} )
+		( 
+			cd "$i" && git log ${i}-${FROM}..${TO:+${i}-${TO}} 
+		)
 	fi
 done | egrep -o 'DEPLOYIT(PB)?-[0-9]+' | sed s/DEPLOYIT-/DEPLOYITPB-/g | sort -u
