dae3d27388809361b27e18cf1239c2b7d6693536
Jasper Stein <jstein@xebialabs.com>
2014-11-07 11:46:25 +0100

    Refactoring CheckGithubFile task

--- dependency-updater/buildSrc/src/main/groovy/com/xebialabs/dependencyUpdater/CheckGithubFile.groovy
+++ dependency-updater/buildSrc/src/main/groovy/com/xebialabs/dependencyUpdater/CheckGithubFile.groovy
@@ -3,18 +3,31 @@ import org.gradle.api.tasks.TaskAction
 class CheckGithubFile extends GitHubTask {
   def repositories = []
   Closure check
+  def nonMatching = []
+
+  def mismatch(repository, key, touchstoneValue, current) {
+    nonMatching += [[repository, key, touchstoneValue, current]]
+  }
 
   @TaskAction
   def check() {
     checkBranch()
+
     repositories.each { repository ->
       def content = ""
       doWithRepo(repository, { url, urlContent, sha ->
         content = urlContent
         logger.debug("Loaded from '{}': '{}'", url, content)
       })
-
       check repository, content
+    }
+
+    if (nonMatching.size() > 0) {
+      println ""
+      project.ext.numInconsistencies += nonMatching.size()
+      nonMatching.each { repo, key, touchstoneValue, value ->
+        println "Repository $repo: $key was $value while $touchstoneValue was expected"
+      }
     }
   }
 }
